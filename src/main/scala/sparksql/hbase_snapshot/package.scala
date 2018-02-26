e9a236a4cfb4247a10cf9428e1ead7e0013c7ab1
yingjie.duan <yingjie.duan@starlight-sms.com>
2018-02-26 15:14:27 +0800

    update

--- src/main/scala/sparksql/hbase_snapshot/package.scala
+++ src/main/scala/sparksql/hbase_snapshot/package.scala
@@ -9,7 +9,7 @@ package object hbase_snapshot {
 
   private[hbase_snapshot] val LOG = LoggerFactory.getLogger("hbase_snapshot")
 
-  abstract class SchemaField extends Serializable
+  private[hbase_snapshot] abstract class SchemaField extends Serializable
 
   private[hbase_snapshot] case class RegisteredSchemaField(fieldName: String, fieldType: String) extends SchemaField with Serializable {
     override def toString: String = s"[fieldName: $fieldName, fieldType: $fieldType]"
@@ -21,17 +21,17 @@ package object hbase_snapshot {
 
   private[hbase_snapshot] case class SchemaType(dataType: DataType, nullable: Boolean)
 
-  protected[hbase_snapshot] val SPARK_SQL_TABLE_SCHEMA  = "sparksql.table.schema"
-  protected[hbase_snapshot] val HBASE_ZOOKEEPER_QUORUM  = HConstants.ZOOKEEPER_QUORUM
-  protected[hbase_snapshot] val HBASE_TABLE_NAME        = "hbase.table.name"
-  protected[hbase_snapshot] val HBASE_TABLE_SCHEMA      = "hbase.table.schema"
-  protected[hbase_snapshot] val HBASE_ROOTDIR           = "hbase.rootdir"
-  protected[hbase_snapshot] val HBASE_SNAPSHOT_VERSIONS = "hbase.snapshot.versions"
-  protected[hbase_snapshot] val HBASE_SNAPSHOT_PATH     = "hbase.snapshot.path"
-  protected[hbase_snapshot] val HBASE_SNAPSHOT_NAME     = "hbase.snapshot.name"
+  private[hbase_snapshot] val SPARK_SQL_TABLE_SCHEMA  = "sparksql.table.schema"
+  private[hbase_snapshot] val HBASE_ZOOKEEPER_QUORUM  = HConstants.ZOOKEEPER_QUORUM
+  private[hbase_snapshot] val HBASE_TABLE_NAME        = "hbase.table.name"
+  private[hbase_snapshot] val HBASE_TABLE_SCHEMA      = "hbase.table.schema"
+  private[hbase_snapshot] val HBASE_ROOTDIR           = "hbase.rootdir"
+  private[hbase_snapshot] val HBASE_SNAPSHOT_VERSIONS = "hbase.snapshot.versions"
+  private[hbase_snapshot] val HBASE_SNAPSHOT_PATH     = "hbase.snapshot.path"
+  private[hbase_snapshot] val HBASE_SNAPSHOT_NAME     = "hbase.snapshot.name"
 
-  protected[hbase_snapshot] val defaultSnapshotVersions = "3"
-  protected[hbase_snapshot] val defaultHBaseRootDir     = "/hbase"
+  private[hbase_snapshot] val defaultSnapshotVersions = "3"
+  private[hbase_snapshot] val defaultHBaseRootDir     = "/hbase"
 
   /**
     * Adds a method, `hbaseTable`, to SQLContext that allows reading data stored in hbase table.
